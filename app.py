c4e1fe30ce09eb0ba429315816eb525e6214a85a
ZeyXs <basile.gamer@gmail.com>
2022-11-14 12:41:07 +0100

    Request to NCBI

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hello, Mec!")
+from Bio import SeqRecord, SeqIO
+from Bio import Entrez
 
-if __name__ == "__main__":
-    main()
+Entrez.email='x@yahoo.us'
+# Définition d'un mail obligatoire pour utiliser Entrez
+req="(SARS-CoV-2 [orgn] AND refseq [filter]) OR (Bat coronavirus RaTG13) OR (MP789 MT121216)"
+#Définition de la reqête pour gagner de la place sur l'écran.
+ficXML=Entrez.esearch(db="nucleotide", term= req)
+#recherche des éléments sur NCBI
+dic=Entrez.read(ficXML)
+idlist=dic["IdList"]
+#Lecture du fichier et extraction des id
+ficXML.close()
+#fermeture de la recherche
+ficSeq=Entrez.efetch(db='nucleotide',id=idlist, rettype='gb')
+#Récupération des fichiers voulu sur NCBI 
+Seqtemp=SeqIO.parse(ficSeq,'gb')
+ficSeq.close()
+#fermeture du efetch
+SeqIO.write(Seqtemp,"seq_covid.gb",'genbank')
+#Lecture des résultats et création du fichier ganbank voulu 
