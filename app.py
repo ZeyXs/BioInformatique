965493621c65700f6a135e18fbb1ee8fde49e684
ZeyXs <basile.gamer@gmail.com>
2022-11-14 12:49:21 +0100

    CORRECTEUR ORTHOGRPAHIQUE

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from Bio import SeqRecord, SeqIO
+from Bio import SeqIO
 from Bio import Entrez
 
-Entrez.email='x@yahoo.us'
+Entrez.email = 'x@yahoo.us'
 # Définition d'un mail obligatoire pour utiliser Entrez
-req="(SARS-CoV-2 [orgn] AND refseq [filter]) OR (Bat coronavirus RaTG13) OR (MP789 MT121216)"
-#Définition de la reqête pour gagner de la place sur l'écran.
-ficXML=Entrez.esearch(db="nucleotide", term= req)
-#recherche des éléments sur NCBI
-dic=Entrez.read(ficXML)
-idlist=dic["IdList"]
+req = "(SARS-CoV-2 [orgn] AND refseq [filter]) OR (Bat coronavirus RaTG13) OR (MP789 MT121216)"
+# Définition de la requête dans une variable
+fic_xml = Entrez.esearch(db="Nucleotide", term=req)
+# Recherche des éléments sur le portail web du NCBI
+dic = Entrez.read(fic_xml)
+id_list = dic["IdList"]
 #Lecture du fichier et extraction des id
-ficXML.close()
+fic_xml.close()
 #fermeture de la recherche
-ficSeq=Entrez.efetch(db='nucleotide',id=idlist, rettype='gb')
+fic_seq = Entrez.efetch(db="Nucleotide", id=id_list, rettype="gb")
 #Récupération des fichiers voulu sur NCBI 
-Seqtemp=SeqIO.parse(ficSeq,'gb')
-ficSeq.close()
+seq_temp = SeqIO.parse(fic_seq, "gb")
+fic_seq.close()
 #fermeture du efetch
-SeqIO.write(Seqtemp,"seq_covid.gb",'genbank')
+SeqIO.write(seq_temp, "seq_covid.gb", "genbank")
 #Lecture des résultats et création du fichier ganbank voulu 
